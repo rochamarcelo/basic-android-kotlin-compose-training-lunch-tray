3d90b8215002e8b359049e5fd16c848266d48864
Marcelo Rocha <contato@omarcelo.com.br>
2025-05-10 12:06:33 -0300

    Add screen enum

--- app/src/main/java/com/example/lunchtray/datasource/ScreenRoute.kt
+++ app/src/main/java/com/example/lunchtray/datasource/ScreenRoute.kt
@@ -1,9 +1,12 @@
 package com.example.lunchtray.datasource
 
-enum class ScreenRoute {
-    Start,
-    EntreeMenu,
-    SideDishMenu,
-    AccompanimentMenu,
-    Checkout
+import androidx.annotation.StringRes
+import com.example.lunchtray.R
+
+enum class ScreenRoute(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    EntreeMenu(title = R.string.choose_entree),
+    SideDishMenu(title = R.string.choose_side_dish),
+    AccompanimentMenu(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout),
 }
